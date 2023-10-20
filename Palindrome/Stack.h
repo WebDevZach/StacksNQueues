b108487bccb4799d08f7bb70bfb9ea13962e711f
Zachary Leyes <leyeszach@gmail.com>
2023-10-20 18:25:27 -0400

    finished stack

--- Palindrome/Stack.h
+++ Palindrome/Stack.h
@@ -22,16 +22,43 @@ bool Stack::push(char myChar)
 	myNode.nextNode = this->top;
 	top = myNode.nextNode;
 
+	if (isEmpty())
+		return false;
+
 	return true;
 }
 
 bool Stack::pop()
 {
+	if (isEmpty()) 
+		return false;
+	
+
 	Node* temp = top;
 	top = top->nextNode;
+
 	delete temp;
+
+	return true;
 }
 
+bool Stack::isEmpty()
+{
+	if (top == nullptr)
+		return true;
+	else
+		return false;
+}
+
+char Stack::peek()
+{
+	if (isEmpty)
+	{
+		return;
+	}
+
+	return top->value;
+}
 
 
 
